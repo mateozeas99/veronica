74f148a1befc0a3e363e23b3c29c39c24b489f8d
rolan <rolando.roc@gmail.com>
2019-03-25 15:26:37 -0600

    Update veronica-sql

--- sql/veronica_schema.sql
+++ sql/veronica_schema.sql
@@ -246,10 +246,24 @@ CREATE SEQUENCE public.invoice_seq
 ALTER TABLE public.invoice_seq OWNER TO postgres;
 
 --
--- Name: with_holding_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+-- Name: payment_method; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE SEQUENCE public.with_holding_seq
+CREATE TABLE public.payment_method (
+    payment_method_id integer NOT NULL,
+    code character varying(3) NOT NULL,
+    description character varying(50) NOT NULL
+);
+
+
+ALTER TABLE public.payment_method OWNER TO postgres;
+
+--
+-- Name: payment_method_payment_method_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.payment_method_payment_method_id_seq
+    AS integer
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -257,14 +271,105 @@ CREATE SEQUENCE public.with_holding_seq
     CACHE 1;
 
 
-ALTER TABLE public.with_holding_seq OWNER TO postgres;
+ALTER TABLE public.payment_method_payment_method_id_seq OWNER TO postgres;
 
 --
--- Name: with_holding; Type: TABLE; Schema: public; Owner: postgres
+-- Name: payment_method_payment_method_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
-CREATE TABLE public.with_holding (
-    with_holding_id integer DEFAULT nextval('public.with_holding_seq'::regclass) NOT NULL,
+ALTER SEQUENCE public.payment_method_payment_method_id_seq OWNED BY public.payment_method.payment_method_id;
+
+
+--
+-- Name: tax_type; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.tax_type (
+    tax_type_id integer NOT NULL,
+    code character varying(3) NOT NULL,
+    description character varying(80) NOT NULL
+);
+
+
+ALTER TABLE public.tax_type OWNER TO postgres;
+
+--
+-- Name: tax_type_tax_type_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.tax_type_tax_type_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.tax_type_tax_type_id_seq OWNER TO postgres;
+
+--
+-- Name: tax_type_tax_type_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.tax_type_tax_type_id_seq OWNED BY public.tax_type.tax_type_id;
+
+
+--
+-- Name: withheld_receipt_type; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.withheld_receipt_type (
+    withheld_receipt_type_id integer NOT NULL,
+    code character varying(3) NOT NULL,
+    description character varying(80) NOT NULL
+);
+
+
+ALTER TABLE public.withheld_receipt_type OWNER TO postgres;
+
+--
+-- Name: withheld_receipt_type_withheld_receipt_type_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.withheld_receipt_type_withheld_receipt_type_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.withheld_receipt_type_withheld_receipt_type_id_seq OWNER TO postgres;
+
+--
+-- Name: withheld_receipt_type_withheld_receipt_type_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.withheld_receipt_type_withheld_receipt_type_id_seq OWNED BY public.withheld_receipt_type.withheld_receipt_type_id;
+
+
+--
+-- Name: withholding_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.withholding_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.withholding_seq OWNER TO postgres;
+
+--
+-- Name: withholding; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.withholding (
+    withholding_id integer DEFAULT nextval('public.withholding_seq'::regclass) NOT NULL,
     access_key character varying(50) NOT NULL,
     sri_version character varying(5) NOT NULL,
     xml_content xml,
@@ -278,7 +383,7 @@ CREATE TABLE public.with_holding (
 );
 
 
-ALTER TABLE public.with_holding OWNER TO postgres;
+ALTER TABLE public.withholding OWNER TO postgres;
 
 --
 -- Name: digital_cert digital_cert_id; Type: DEFAULT; Schema: public; Owner: postgres
@@ -299,6 +404,103 @@ ALTER TABLE ONLY public.internal_status ALTER COLUMN internal_status_id SET DEFA
 --
 
 ALTER TABLE ONLY public.invoice ALTER COLUMN invoice_id SET DEFAULT nextval('public.invoice_invoice_id_seq'::regclass);
+
+
+--
+-- Name: payment_method payment_method_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.payment_method ALTER COLUMN payment_method_id SET DEFAULT nextval('public.payment_method_payment_method_id_seq'::regclass);
+
+
+--
+-- Name: tax_type tax_type_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.tax_type ALTER COLUMN tax_type_id SET DEFAULT nextval('public.tax_type_tax_type_id_seq'::regclass);
+
+
+--
+-- Name: withheld_receipt_type withheld_receipt_type_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.withheld_receipt_type ALTER COLUMN withheld_receipt_type_id SET DEFAULT nextval('public.withheld_receipt_type_withheld_receipt_type_id_seq'::regclass);
+
+--
+-- Name: bol_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.bol_seq', 13, true);
+
+
+--
+-- Name: consignne_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.consignne_seq', 12, true);
+
+
+--
+-- Name: digital_cert_digital_cert_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.digital_cert_digital_cert_id_seq', 1, false);
+
+
+--
+-- Name: digital_cert_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.digital_cert_seq', 2, true);
+
+
+--
+-- Name: internal_status_internal_status_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.internal_status_internal_status_id_seq', 1, false);
+
+
+--
+-- Name: invoice_invoice_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.invoice_invoice_id_seq', 1, false);
+
+
+--
+-- Name: invoice_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.invoice_seq', 1, true);
+
+
+--
+-- Name: payment_method_payment_method_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.payment_method_payment_method_id_seq', 9, true);
+
+
+--
+-- Name: tax_type_tax_type_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.tax_type_tax_type_id_seq', 5, true);
+
+
+--
+-- Name: withheld_receipt_type_withheld_receipt_type_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.withheld_receipt_type_withheld_receipt_type_id_seq', 13, true);
+
+
+--
+-- Name: withholding_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.withholding_seq', 1, true);
 
 
 --
@@ -366,19 +568,43 @@ ALTER TABLE ONLY public.invoice
 
 
 --
--- Name: with_holding with_holding_access_key_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: payment_method payment_method_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public.with_holding
-    ADD CONSTRAINT with_holding_access_key_key UNIQUE (access_key);
+ALTER TABLE ONLY public.payment_method
+    ADD CONSTRAINT payment_method_pkey PRIMARY KEY (payment_method_id);
 
 
 --
--- Name: with_holding with_holding_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: tax_type tax_type_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public.with_holding
-    ADD CONSTRAINT with_holding_pkey PRIMARY KEY (with_holding_id);
+ALTER TABLE ONLY public.tax_type
+    ADD CONSTRAINT tax_type_pkey PRIMARY KEY (tax_type_id);
+
+
+--
+-- Name: withheld_receipt_type withheld_receipt_type_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.withheld_receipt_type
+    ADD CONSTRAINT withheld_receipt_type_pkey PRIMARY KEY (withheld_receipt_type_id);
+
+
+--
+-- Name: withholding withholding_access_key_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.withholding
+    ADD CONSTRAINT withholding_access_key_key UNIQUE (access_key);
+
+
+--
+-- Name: withholding withholding_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.withholding
+    ADD CONSTRAINT withholding_pkey PRIMARY KEY (withholding_id);
 
 
 --
@@ -406,11 +632,11 @@ ALTER TABLE ONLY public.invoice
 
 
 --
--- Name: with_holding with_holding_internal_status_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: withholding withholding_internal_status_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public.with_holding
-    ADD CONSTRAINT with_holding_internal_status_id_fkey FOREIGN KEY (internal_status_id) REFERENCES public.internal_status(internal_status_id);
+ALTER TABLE ONLY public.withholding
+    ADD CONSTRAINT withholding_internal_status_id_fkey FOREIGN KEY (internal_status_id) REFERENCES public.internal_status(internal_status_id);
 
 
 --
